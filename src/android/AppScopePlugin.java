ebb2ea19e23d15a67d811974eed4399c2c7ced15
Darryl Pogue <darryl@ayogo.com>
2024-01-26 11:39:31 -0800

    [Android] Use correct prefix for remapping URLs
    This needs to support the scheme/hostname prefix on newer versions of
    Cordova Android.
    
    Closes #16.

--- src/android/AppScopePlugin.java
+++ src/android/AppScopePlugin.java
@@ -15,7 +15,6 @@
  */
 package com.ayogo.cordova.appscope;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
@@ -24,6 +23,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.LOG;
 
 
@@ -123,38 +123,87 @@ public class AppScopePlugin extends CordovaPlugin {
             remapped = "index.html" + remapped;
         }
 
-        String resultURL = "file:///android_asset/www/" + remapped;
+        String resultURL = getLaunchUrlPrefix() + remapped;
 
+        String codePushPrefix = tryFindCodePushPrefix();
+        if (codePushPrefix != null) {
+            resultURL = codePushPrefix + remapped;
+        }
+
+        LOG.d(TAG, "Result URL is " + resultURL);
+        return Uri.parse(resultURL);
+    }
+
+    /**
+     * Get the default prefix for Cordova web assets.
+     *
+     * This needs to determine whether we're serving from a file:/// URL or
+     * from a scheme/hostname, and return the correct prefix for the remapped
+     * URL.
+     */
+    private String getLaunchUrlPrefix() {
+        int major = 0;
         try {
-            CordovaPlugin codepush = this.webView.getPluginManager().getPlugin("CodePush");
-            if (codepush != null) {
-                Class<?> codepushClass = codepush.getClass();
-                Field pkgMgr = codepushClass.getDeclaredField("codePushPackageManager");
-                pkgMgr.setAccessible(true);
+            major = Integer.parseInt(CordovaWebView.CORDOVA_VERSION.split("\\.", 2)[0]);
+        } catch (Exception e) {
+            /* Ignore, let major default to 0 */
+        }
 
-                Object codePushPackageManager = pkgMgr.get(codepush);
+        // Before Cordova Android 10, we always use file:/// URLs
+        if ((major > 0 && major < 10) || preferences.getBoolean("AndroidInsecureFileModeEnabled", false)) {
+            return "file:///android_asset/www/";
+        } else {
+            String scheme = preferences.getString("scheme", "https").toLowerCase();
+            String hostname = preferences.getString("hostname", "localhost").toLowerCase();
 
-                Class<?> cppmClass = pkgMgr.getType();
+            if (!scheme.contentEquals("http") && !scheme.contentEquals("https")) {
+                scheme = "https";
+            }
 
-                Method getCurrentPackageMetadata = cppmClass.getDeclaredMethod("getCurrentPackageMetadata");
+            return scheme + "://" + hostname + '/';
+        }
+    }
 
-                Object packageMetadata = getCurrentPackageMetadata.invoke(codePushPackageManager);
+    /**
+     * Try to find a CodePush path prefix for the current version.
+     *
+     * This conditionally checks if the CodePush plugin is installed and then
+     * tries to look up the directory for the current CodePush package version,
+     * so that the right prefix can be used for a remapped URL within the
+     * CodePush package.
+     */
+    private String tryFindCodePushPrefix() {
+        try {
+            CordovaPlugin codePush = this.webView.getPluginManager().getPlugin("CodePush");
+            if (codePush == null) {
+                return null;
+            }
 
-                if (packageMetadata != null) {
-                    Class<?> metadataClass = getCurrentPackageMetadata.getReturnType();
-                    Field localPathField = metadataClass.getDeclaredField("localPath");
-                    String localPath = (String)localPathField.get(packageMetadata);
+            Class<?> codepushClass = codePush.getClass();
+            Field pkgMgr = codepushClass.getDeclaredField("codePushPackageManager");
+            pkgMgr.setAccessible(true);
 
-                    if (localPath != null) {
-                        resultURL = "file://" + this.cordova.getActivity().getFilesDir() + localPath + "www/" + remapped;
-                    }
+            Object codePushPackageManager = pkgMgr.get(codePush);
+
+            Class<?> cppmClass = pkgMgr.getType();
+
+            Method getCurrentPackageMetadata = cppmClass.getDeclaredMethod("getCurrentPackageMetadata");
+
+            Object packageMetadata = getCurrentPackageMetadata.invoke(codePushPackageManager);
+
+            if (packageMetadata != null) {
+                Class<?> metadataClass = getCurrentPackageMetadata.getReturnType();
+                Field localPathField = metadataClass.getDeclaredField("localPath");
+                String localPath = (String)localPathField.get(packageMetadata);
+
+                if (localPath != null) {
+                    return "file://" + this.cordova.getActivity().getFilesDir() + localPath + "www/";
                 }
             }
         } catch (Exception e) {
           LOG.e(TAG, Log.getStackTraceString(e));
         }
 
-        LOG.d(TAG, "Result URL is " + resultURL);
-        return Uri.parse(resultURL);
+        return null;
     }
 }
